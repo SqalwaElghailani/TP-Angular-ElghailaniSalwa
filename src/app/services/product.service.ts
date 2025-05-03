2ce13e33f62fabd0a52a67f0622b50707e2bd541
SqalwaElghailani <salwaelghailani8@gmail.com>
2025-05-03 20:41:00 +0200

    Mise à jour des services :Refonte du ProductService pour l'API

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,53 +1,48 @@
 import { Injectable } from '@angular/core';
 import { Product } from '../models/product.model';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map, Observable, of } from 'rxjs';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-  private products: Product[] = [
-    {
-      id: 1,
-      name: 'Ordinateur Portable',
-      description: 'PC portable 15 pouces, 16GB RAM, 512GB SSD',
-      price: 899.99,
-      quantity: 15,
-      imageUrl: 'assets/images/laptop.jpg'
-    },
-    {
-      id: 2,
-      name: 'Smartphone',
-      description: 'Smartphone 6.5 pouces, 128GB, 4 caméras',
-      price: 599.99,
-      quantity: 8,
-      imageUrl: 'assets/images/phone.jpg'
-    },
-    {
-      id: 3,
-      name: 'Casque Bluetooth',
-      description: 'Casque sans fil avec réduction de bruit',
-      price: 199.99,
-      quantity: 0,
-      imageUrl: 'assets/images/headphones.jpg'
-    },
-    {
-      id: 4,
-      name: 'Souris Gaming',
-      description: 'Souris ergonomique avec 6 boutons programmables',
-      price: 49.99,
-      quantity: 5,
-      imageUrl: 'assets/images/mouse.jpg'
-    }
-  ];
+  private baseUrl = 'http://localhost:3000/api/products'; 
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
 
-  getProducts(): Product[] {
-    return this.products.filter(product => product.quantity > 0);
+  getProducts(): Observable<Product[]> {
+    return this.http.get<any[]>(this.baseUrl).pipe(
+      map(apiProducts => apiProducts.map(apiProduct => ({
+        id: apiProduct.productID,
+        name: apiProduct.productTitle,
+        price: apiProduct.productPrice,
+        quantity: apiProduct.quantity,
+        imageUrl: `assets/images/${apiProduct.productImage}`,
+        description: apiProduct.category
+      }))),
+      catchError(error => {
+        console.error('Erreur:', error);
+        return of([]);
+      })
+    );
   }
-
-  getProductById(id: number): Product | undefined {
-    return this.products.find(product => product.id === id);
+  
+  getProductById(id: number): Observable<Product | undefined> { // ID en string
+    return this.http.get<any>(`${this.baseUrl}/${id}`).pipe(
+      map(apiProduct => ({
+        id: apiProduct.productID,
+        name: apiProduct.productTitle,
+        price: apiProduct.productPrice,
+        quantity: apiProduct.quantity,
+        imageUrl: `assets/images/${apiProduct.productImage}`,
+        description: apiProduct.category
+      })),
+      catchError(error => {
+        console.error('Erreur:', error);
+        return of(undefined);
+      })
+    );
   }
 }
