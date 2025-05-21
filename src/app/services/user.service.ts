03a4a3e78f31fe394a1d5d41eb4facbc10012808
SqalwaElghailani <salwaelghailani8@gmail.com>
2025-05-22 00:07:37 +0200

    implémentation du service d'authentification

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,8 +1,9 @@
 // user.service.ts
-import { Injectable } from '@angular/core';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, catchError, map, BehaviorSubject, of } from 'rxjs';
 import { SafeUser, User } from '../models/user.model';
+import { isPlatformBrowser } from '@angular/common';
 
 
 @Injectable({
@@ -13,26 +14,35 @@ export class UserService {
   private currentUserSubject = new BehaviorSubject<SafeUser | null>(null);
   public currentUser$ = this.currentUserSubject.asObservable();
 
-  constructor(private http: HttpClient) {
-    // Vérifier le localStorage au chargement du service
-    const storedUser = localStorage.getItem('currentUser');
-    if (storedUser) {
-      this.currentUserSubject.next(JSON.parse(storedUser));
+  private isBrowser: boolean;
+
+  constructor(
+    private http: HttpClient,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {
+    this.isBrowser = isPlatformBrowser(this.platformId);
+
+    if (this.isBrowser) {
+      const storedUser = localStorage.getItem('currentUser');
+      if (storedUser) {
+        this.currentUserSubject.next(JSON.parse(storedUser));
+      }
     }
   }
 
   signIn(email: string, password: string): Observable<SafeUser | null> {
     return this.http.post<User>(this.apiUrl, { email, password }).pipe(
       map(user => {
-        // Créer un objet utilisateur sécurisé sans le mot de passe
         const safeUser: SafeUser = {
           firstName: user.firstName,
           lastName: user.lastName,
           email: user.email
         };
-        
-        // Stocker l'utilisateur dans le localStorage et le subject
-        localStorage.setItem('currentUser', JSON.stringify(safeUser));
+
+        if (this.isBrowser) {
+          localStorage.setItem('currentUser', JSON.stringify(safeUser));
+        }
+
         this.currentUserSubject.next(safeUser);
         return safeUser;
       }),
@@ -44,8 +54,9 @@ export class UserService {
   }
 
   signOut(): void {
-    // Supprimer l'utilisateur du localStorage et du subject
-    localStorage.removeItem('currentUser');
+    if (this.isBrowser) {
+      localStorage.removeItem('currentUser');
+    }
     this.currentUserSubject.next(null);
   }
 
