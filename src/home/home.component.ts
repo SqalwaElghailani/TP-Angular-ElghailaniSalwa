e1eafb1f57930ab38455a15bee10be858f9a835e
SqalwaElghailani <salwaelghailani8@gmail.com>
2025-06-16 17:53:02 +0200

    Amélioration du design de la page d'accueil pour mieux s'adapter à l'identité de l'application

--- src/home/home.component.ts
+++ src/home/home.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { CardService } from '../app/services/card.service';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
   imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  cartCount: number = 0;
 
+  constructor(
+    private cartService: CardService,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      const userId = localStorage.getItem('userId');
+      if (userId) {
+        this.cartService.getUserCart(+userId).subscribe(data => {
+          this.cartCount = data.length;
+        });
+      }
+    }
+  }
 }
+
