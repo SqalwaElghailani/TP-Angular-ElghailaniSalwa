71c8ae566cb199a065b4d9770234f88e400455fb
SqalwaElghailani <salwaelghailani8@gmail.com>
2025-06-16 18:07:35 +0200

    Mise à jour du composant Catalog : ajout de nouveaux produits et adaptation du style pour une meilleure intégration dans l'application

--- src/catalog/catalog.component.ts
+++ src/catalog/catalog.component.ts
@@ -5,6 +5,7 @@ import { Product } from '../app/models/product.model';
 import { ProductService } from '../app/services/product.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { RouterModule } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-catalog',
@@ -16,9 +17,12 @@ import { RouterModule } from '@angular/router';
 export class CatalogComponent implements OnInit {
 
   products: Product[] = [];
+  topProducts: Product[] = [];
+  activeTopIndex = 0;
   selectedProductId: number | null = null;
-
+ Math = Math;
   constructor(
+    private http: HttpClient,
     private productService: ProductService,
     private router: Router,
     private route: ActivatedRoute
@@ -26,14 +30,27 @@ export class CatalogComponent implements OnInit {
 
   ngOnInit(): void {
     this.productService.getProducts().subscribe(
-      products => this.products = products,
-      error => console.error('erreur :', error)
+       products => {
+        this.products = products;
+        this.topProducts = products.filter(p => p.rating >= 9);
+        
+      },
+      error => console.error('erreur :', error),
+      
     );
 
     this.route.queryParams.subscribe(params => {
       const id = params['product'];
       this.selectedProductId = id ? +id : null;
     });
+  }
+  nextTopProduct() {
+    this.activeTopIndex = (this.activeTopIndex + 1) % this.topProducts.length;
+  }
+
+  prevTopProduct() {
+    this.activeTopIndex =
+      (this.activeTopIndex - 1 + this.topProducts.length) % this.topProducts.length;
   }
 
   viewDetails(productId: number): void {
@@ -52,4 +69,34 @@ export class CatalogComponent implements OnInit {
     const imgElement = event.target as HTMLImageElement;
     imgElement.src = '/assets/images/default-product.png';
   }
+
+  getRoundedStars(rating: number): number {
+  return Math.floor(rating);
+}
+  getDiscount(oldPrice: number, newPrice: number): number {
+    return Math.round(((oldPrice - newPrice) / oldPrice) * 100);
+  }
+  
+
+
+
+
+
+  addToCart(productId: number) {
+  const userId = localStorage.getItem('userId');
+  const date = new Date().toISOString();
+
+  if (!userId) {
+    alert("⛔ Vous devez vous connecter !");
+    return;
+  }
+
+  const data = { userId, productId, date };
+
+  this.http.post('http://localhost:3000/api/cart', data).subscribe({
+    next: () => alert("✅ Produit ajouté au panier"),
+    error: () => alert("❌ Erreur lors de l'ajout")
+  });
+}
+
 }
