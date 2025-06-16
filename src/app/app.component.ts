d062491ae16ef1609bb102da9a10940ff77e9501
SqalwaElghailani <salwaelghailani8@gmail.com>
2025-06-16 17:59:19 +0200

    Adaptation du composant principal (AppComponent) à l'application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,29 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { CatalogComponent } from '../catalog/catalog.component';
+import { NavigationEnd, RouterOutlet,Router } from '@angular/router';
+import { HomeComponent } from '../home/home.component';
+
+
 
 @Component({
   selector: 'app-root',
   
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Tp5';
+
+  showHomeComponent: boolean = true;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        // هنا كتخبي app-home فصفحات login مثلاً
+        this.showHomeComponent = !['/home', '/signin'].includes(event.url);
+      }
+    });
+  }
 }
