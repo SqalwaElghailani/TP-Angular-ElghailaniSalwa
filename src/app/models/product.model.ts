ad400b77f3b1f01bebd72865474b6baf4ccf4829
SqalwaElghailani <salwaelghailani8@gmail.com>
2025-06-16 18:00:44 +0200

    Modification du modèle Product pour refléter les nouvelles données

--- src/app/models/product.model.ts
+++ src/app/models/product.model.ts
@@ -3,6 +3,13 @@ export interface Product {
     name: string;
     description: string;
     price: number;
+      oldPrice?: number; // pour afficher le prix barré
     quantity: number;
     imageUrl?: string;
+    
+    author: string;
+    genre: string;
+    rating: number;
+    chapters: number;
+    fullSummary: string;
   }
