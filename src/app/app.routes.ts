5baa73f4e3ee052ce7735f6219cb18587712ce36
SqalwaElghailani <salwaelghailani8@gmail.com>
2025-06-17 20:41:54 +0200

    Mise à jour du routing : ajout de la route vers la page de commande

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,21 +5,19 @@ import { SigninComponent } from "../signin/signin.component";
 import { HomeComponent } from '../home/home.component';
 import { SignupComponent } from '../signup/signup.component';
 import { CardComponent } from '../card/card.component';
+import { CommendeComponent } from '../commende/commende.component';
 
 export const routes: Routes = [
     {path : 'home', component : HomeComponent, title: 'My Catalog products'},
     {path : 'Catalog', component : CatalogComponent, title: 'My Catalog products'},
     {path : 'card', component : CardComponent, title: 'My Card'},
-    {path: 'product-details/:id',
-  component: ProductDetailsComponent,
-  title: 'Product details'
-},
-
-
+    {path: 'product-details/:id',component: ProductDetailsComponent,title: 'Product details'},
+    {path : 'commende/:id', component : CommendeComponent, title: 'My Commende'},
+    {path : 'commende', component : CommendeComponent, title: 'My Commende'},
    
     {path : 'signin', component : SigninComponent, title: 'My signin page'},
     {path : 'signup', component : SignupComponent, title: 'My signin page'},
-    {path : '', redirectTo : '/home', pathMatch : 'full'},
+    {path : '', redirectTo : '/Catalog', pathMatch : 'full'},
     
 
 
